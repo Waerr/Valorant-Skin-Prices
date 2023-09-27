15e6f1972c355eea132ec9d588f9d8f80de74144
Francisco Rodrigues <waerdev@gmail.com>
2023-09-27 14:34:50 +0100

    Fix Legacy table not retrieving prices

--- utils/getSkins.py
+++ utils/getSkins.py
@@ -1,6 +1,6 @@
 import requests, lxml, re
 from bs4 import BeautifulSoup
-
+import time
 
 def getPrice():
     """HANDLES THE PRICE OF ALL THE SKINS IN VP"""
@@ -9,16 +9,20 @@ def getPrice():
     vp_url = "https://valorant.fandom.com/wiki/Weapon_Skins"
     vp_html = requests.get(vp_url).text
     vp_soup = BeautifulSoup(vp_html, "lxml")
-
-    # Find the tables with class wikitable and extract the prices from the 6th column of each row
+    
+    # Find the tables with class fandom-table and extract the prices from the 6th column of each row
     vp_rows = vp_soup.select("table.fandom-table")[0].find_all("tr")[1:]
     vp_prices = [re.sub("[\xa0\n,]", "", row.find_all("td")[4].text.strip()) for row in vp_rows]
     vp_prices = [int(price) for price in vp_prices]
 
-    # Legacy items are those not returning to back to store eg. "Champions Bundle"
-    # Find the table with class sortable and extract the prices from the Notes column
-    legacy_rows = vp_soup.select("table.sortable")[4].find_all("tr")[1:]
-    legacy_prices = [int(re.findall("\d{4}$", row.find_all("td")[3].text.strip())[0]) for row in legacy_rows if re.findall("\d{4}$", row.find_all("td")[3].text.strip())]
+    # Legacy items are those not returning back to the store eg. "Champions Bundle"
+    # Find the table with class fandom-table and extract the prices from the Notes column
+    legacy_table = vp_soup.find_all("div", class_="wds-tab__content")[3]
+    legacy_rows = legacy_table.select("table.fandom-table")[0].find_all("tr")[1:]
+    # Regex to find the Price from the Notes Column as an int
+    legacy_prices = [int(''.join(filter(str.isdigit, re.findall(r'\d{1,3}(?:,\d{3})+', 
+                    row.find_all("td")[3].text.strip())[0]))) 
+                    for row in legacy_rows if re.findall(r'\d{1,3}(?:,\d{3})+', row.find_all("td")[3].text.strip())]  
     legacy_prices = [int(price) for price in legacy_prices]
     
     
@@ -26,6 +30,5 @@ def getPrice():
     return (sum(legacy_prices + vp_prices))
 
 
-    
     
         
